ef474bf52fcc85eb3a4c04f8c76193af8062060f
Ronald2397 <ronaldprzsn@gmail.com>
2026-05-19 23:44:01 -0600

    VirtualBoost initial setup

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+
+      <h1 className="text-6xl font-bold">
+        <span className="text-cyan-400">
+          Virtual
+        </span>
+
+        <span className="text-orange-500">
+          Boost
+        </span>
+      </h1>
+
+      <p className="mt-4 text-zinc-400">
+        Plataforma de branding
+      </p>
+
+    </main>
+  );
+}
